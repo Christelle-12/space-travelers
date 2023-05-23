d41abcdb3eb16abab3b5ec496fa9a02edd4add54
Chritelle-12 <mariechristellenirere@gmail.com>
2023-05-23 13:43:19 +0200

    Add rocket directory

---
+++ src/Redux/Rockets/RocketsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+});
+
+export default rocketsSlice.reducer;
