fd5a066908a67817b5c517fa01e2b7c67fb4259c
Benson Njuguna <njugunab655@gmail.com>
2023-05-23 18:15:55 +0300

    Set data based on state

--- src/components/Missions/Mission.js
+++ src/components/Missions/Mission.js
@@ -1,15 +1,40 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions } from '../../Redux/Missions/missionsSlice';
+import MissionRow from './MissionRow';
 
 const Mission = () => {
+  const { data, loading } = useSelector((state) => state.missions);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
 
+  if (loading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
-    <p>hello</p>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <td>Mission</td>
+            <td>Description</td>
+            <td>Status</td>
+            <td> </td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((each) => <MissionRow key={each.id} props={each} />)}
+        </tbody>
+      </table>
+    </div>
   );
 };
 export default Mission;
