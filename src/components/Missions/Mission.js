e8dbe8144f76fd8520713088cbedf0f8b540521a
Benson Njuguna <njugunab655@gmail.com>
2023-05-23 17:06:53 +0300

    Dispatch action to fetch data

--- src/components/Missions/Mission.js
+++ src/components/Missions/Mission.js
@@ -1,4 +1,15 @@
-const Mission = () => (
-  <p>hello</p>
-);
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../../Redux/Missions/missionsSlice';
+
+const Mission = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <p>hello</p>
+  );
+};
 export default Mission;
