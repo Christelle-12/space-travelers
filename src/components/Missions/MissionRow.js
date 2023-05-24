37a1d39c4c2ecefd42c30dbe6a03641b61255267
Benson Njuguna <njugunab655@gmail.com>
2023-05-24 09:50:24 +0300

    Add event handlers

--- src/components/Missions/MissionRow.js
+++ src/components/Missions/MissionRow.js
@@ -1,22 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import styles from '../../css/missionstable.module.css';
+import { joinMission, leaveMission } from '../../Redux/Missions/missionsSlice';
 
 const MissionRow = ({
   props,
 }) => {
-  const { name, description, Reserved } = props;
+  const {
+    id, name, description, joined,
+  } = props;
+
+  const dispatch = useDispatch();
+
   return (
     <tr className={styles.tableRow}>
       <th className={styles.tableCell}>{name}</th>
       <td className={styles.tableCell}>{description}</td>
       <td className={styles.tableCell}>
-        {Reserved === true && <button className={styles.member} type="button">Active Member</button>}
-        {Reserved !== true && <button className={styles.notMember} type="button">NOT A MEMBER</button>}
+        {joined === true
+          && (
+          <button
+            className={styles.member}
+            type="button"
+          >
+            Active Member
+          </button>
+          )}
+        {joined !== true
+          && (
+          <button
+            className={styles.notMember}
+            type="button"
+          >
+            NOT A MEMBER
+          </button>
+          )}
       </td>
       <td className={styles.tableCell}>
-        {Reserved === true && <button className={styles.leave} type="button">Leave Mission</button>}
-        {Reserved !== true && <button className={styles.join} type="button">Join Mission</button>}
+        {joined === true
+          && (
+          <button
+            className={styles.leave}
+            type="button"
+            onClick={() => dispatch(leaveMission(id))}
+          >
+            Leave Mission
+          </button>
+          )}
+        {joined !== true
+          && (
+          <button
+            className={styles.join}
+            type="button"
+            onClick={() => dispatch(joinMission(id))}
+          >
+            Join Mission
+          </button>
+          )}
       </td>
     </tr>
   );
@@ -24,12 +65,14 @@ const MissionRow = ({
 
 MissionRow.propTypes = {
   props: PropTypes.shape({
+    id: PropTypes.string,
     name: PropTypes.string,
     description: PropTypes.string,
   }).isRequired,
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  Reserved: PropTypes.bool.isRequired,
+  joined: PropTypes.bool.isRequired,
 };
 
 export default MissionRow;
