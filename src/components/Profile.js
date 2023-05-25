244bdf020caef908c09ba0f724a10b3f4c0e6d71
Chritelle-12 <mariechristellenirere@gmail.com>
2023-05-25 14:32:48 +0200

    Add my profile page for rocket

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,21 +3,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { leaveMission } from '../Redux/Missions/missionsSlice';
 import styles from '../css/missionstable.module.css';
+import { cancelReserve } from '../Redux/Rockets/RocketsSlice';
 
 const Profile = () => {
   const missionsData = useSelector((state) => state.missions.data);
   const reservedMissions = missionsData.filter((each) => each.reserved === true);
+  const rocketsData = useSelector((state) => state.rockets.data);
+  const reservedRockets = rocketsData.filter((each) => each.reserved === true);
 
   const dispatch = useDispatch();
 
   return (
     <div>
       <table className={styles.profileTable}>
-        <tr>
-          <th>My Missions</th>
-        </tr>
-        {reservedMissions.length < 1
-          ? (
+        <tbody>
+          <tr>
+            <th>My Missions</th>
+          </tr>
+          {reservedMissions.length < 1 ? (
             <tr>
               <td className={styles.grayed}>
                 No missions
@@ -28,22 +31,53 @@ const Profile = () => {
                 </Link>
               </td>
             </tr>
-          )
-          : reservedMissions.map((res) => (
-            <tr key={res.id}>
-              <td className={styles.tableCell}>
-                {res.name}
-                <button
-                  className={styles.leave}
-                  type="button"
-                  onClick={() => dispatch(leaveMission(res.id))}
-                >
-                  Leave Mission
-                </button>
-              </td>
-            </tr>
-          ))}
+          ) : (
+            reservedMissions.map((res) => (
+              <tr key={res.id}>
+                <td className={styles.tableCell}>
+                  {res.name}
+                  <button
+                    className={styles.leave}
+                    type="button"
+                    onClick={() => dispatch(leaveMission(res.id))}
+                  >
+                    Leave Mission
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
       </table>
+      <ul>
+        <li>
+          <h1>My Rockets</h1>
+        </li>
+        {(() => {
+          if (reservedRockets.length < 1) {
+            return (
+              <li>
+                <span>
+                  No rockets reserved
+                  <Link to="/">
+                    <button type="button">
+                      Reserve rocket
+                    </button>
+                  </Link>
+                </span>
+              </li>
+            );
+          }
+          return reservedRockets.map((response) => (
+            <li key={response.id}>
+              <p>{response.name}</p>
+              <button onClick={() => dispatch(cancelReserve(response.id))} type="button">
+                Cancel Reservation
+              </button>
+            </li>
+          ));
+        })()}
+      </ul>
 
     </div>
   );
