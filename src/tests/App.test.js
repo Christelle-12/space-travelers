ae66838144737c43da9fcbca3dcff058ff9dc8ef
Chritelle-12 <mariechristellenirere@gmail.com>
2023-05-26 14:37:59 +0200

    Write Tests for component

---
+++ src/tests/App.test.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../Redux/Store';
+import App from '../App';
+import * as ReactRedux from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useDispatch: jest.fn(),
+  }));
+  
+  test('renders the App component', () => {
+    const useDispatchMock = jest.spyOn(ReactRedux, 'useDispatch');
+    useDispatchMock.mockReturnValue(jest.fn());
+  
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  
+    // Test for the Navigation component
+    const navigationElement = screen.getByText(/Space Travelers' Hub/i);
+    expect(navigationElement).toBeInTheDocument();
+  
+    // Test for the Mission component
+    const missionElement = screen.getByText(/Mission/i);
+    expect(missionElement).toBeInTheDocument();
+  
+    // Test for the Profile component
+    const profileElement = screen.getByText(/My Profile/i);
+    expect(profileElement).toBeInTheDocument();
+  });
